3871ddbfc5ade4fef3915f2f81eb257386ac825d
Tetsuri Moriya <tetsuri.moriya@gmail.com>
2020-07-26 05:59:55 +0900

    temporary commit [ci skip]

--- tfrecord/generate_tfrecord.py
+++ tfrecord/generate_tfrecord.py
@@ -71,7 +71,7 @@ def create_tf_example(group, path):
         ymins.append(row['ymin'] / height)
         ymaxs.append(row['ymax'] / height)
         classes_text.append(row['class'].encode('utf8'))
-        classes.append(class_text_to_int(row['class']))
+        classes.append(class_text_to_int(row['class'].encode('utf8')))
     print(path)
     print(group)
     print(classes)
