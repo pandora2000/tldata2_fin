85f3c35868a90725a64b3d890d3660d95f16ca1d
Tetsuri Moriya <tetsuri.moriya@gmail.com>
2020-07-26 00:29:57 +0900

    temporary commit [ci skip]

---
+++ chunked_commit.py
@@ -0,0 +1,17 @@
+import os
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+dpath = os.path.dirname(os.path.realpath(__file__))
+paths = [f'images/train/{p}' for p in os.listdir(f'{dpath}/images/train')] + [f'images/test/{p}' for p in os.listdir(f'{dpath}/images/test')]
+cs = chunks(paths, 500)
+for i, c in enumerate(cs):
+    com = ' '.join([f"'{x}'" for x in c])
+    print(f'{i + 1} / {len(cs)}')
+    os.system(f'git add {com}')
+    print('added')
+    os.system(f"git commit -m 'temporary commit'")
+    print('committed')
+    os.system('git push origin master')
+    print('pushed')
